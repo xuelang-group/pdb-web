f5ef30a2d61a705ddc3fcbfef8bc2eee7ed6676f
nanaya1 <878493162@qq.com>
2024-03-12 18:18:55 +0800

    feat：独立web

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import './common.less'
-const pinia = createPinia()
-createApp(App).use(pinia).mount('#app')
