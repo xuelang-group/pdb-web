1fa5f279aec9e7dbf357a8cb49af0d94af56d0f1
nanaya1 <878493162@qq.com>
2024-06-21 11:24:08 +0800

    feat：删除节点时，父节点检入检出

--- src/pages/graph/object/index.tsx
+++ src/pages/graph/object/index.tsx
@@ -13,7 +13,7 @@ import store from '@/store';
 import { initG6 } from '@/g6';
 import { edgeLabelStyle } from '@/g6/type/edge';
 import { G6OperateFunctions } from '@/g6/object/behavior';
-import { deleteObjectRelation, getChildren, getRoots, setCommonParams } from '@/actions/object';
+import { checkOutObject, deleteObjectRelation, getChildren, getRoots, setCommonParams } from '@/actions/object';
 import { CustomObjectConfig, Parent, setObjects } from '@/reducers/object';
 import { RelationConfig } from '@/reducers/relation';
 import { QueryResultState, setResult } from '@/reducers/query';
@@ -598,9 +598,41 @@ export default function Editor(props: EditorProps) {
     )
   }
 
-  const handleModalOk = function (currentEditModel: any, removeAll: boolean) {
+  const handleModalOk = async function (currentEditModel: any, removeAll: boolean) {
     setModalLoading(true);
     if (currentEditModel.type === "pdbNode") {
+      const parentId = currentEditModel.parent;
+      if (parentId && parentId !== rootNode?.uid) {
+        const parentNode = graph.findById(parentId);
+        if (parentNode) {
+          const parentData = parentNode.getModel().data;
+          let isCheckout = false;
+          if (parentData && parentData['x.version'] && !parentData['x.checkout']) {
+            await (() => {
+              return new Promise((resolve) => {
+                checkOutObject(parentId, (success: boolean, response: any) => {
+                  resolve(null);
+                  if (success) {
+                    isCheckout = true;
+                    graph.updateItem(parentNode, {
+                      data: {
+                        ...parentData,
+                        'x.checkout': true
+                      }
+                    });
+                  } else {
+                    notification.error({
+                      message: '删除实例失败',
+                      description: response.message || response.msg
+                    });
+                  }
+                });
+              })
+            })();
+            if (!isCheckout) return;
+          }
+        }
+      }
       G6OperateFunctions.removeNode(currentEditModel?.id, {
         uid: currentEditModel?.uid,
         recurse: removeAll
