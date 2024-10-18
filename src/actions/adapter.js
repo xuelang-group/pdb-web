5f2c79abf7c5c3c63d17570c703b9f003608b31c
xushu.zdh <1198674714@qq.com>
2024-10-18 11:07:47 +0800

    从门户跳转过来后，根据requestId过滤有权限的指标

--- src/actions/adapter.js
+++ src/actions/adapter.js
@@ -3,12 +3,22 @@ import axios from '../utils/axios';
 const apiPrefix = '/adapter';
 const api = {
   buzProcess: apiPrefix + '/buzProcess',  // 指标过程域列表
+  pdbIdList: apiPrefix + '/pdbIdList',  // 获取当前用户有权限的指标id列表 
 };
 
 //指标过程域列表
 export const getBuzProcess = (params, callback) => {
   return axios.get(api['buzProcess'],params).then(({ data }) => {
-    callback && callback(true, data);
+    callback && callback(data.success, data);
+  }, (err) => {
+    callback && callback(false, err);
+  }); 
+};
+
+// 获取当前用户有权限的指标id列表
+export const getPdbIdList = (params, callback) => {
+  return axios.get(api['pdbIdList'],params).then(({ data }) => {
+    callback && callback(data.success, data);
   }, (err) => {
     callback && callback(false, err);
   }); 
