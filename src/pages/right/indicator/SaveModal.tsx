5f45f98bfe6f77c7e379d7c086c431932a07e010
xushu.zdh <1198674714@qq.com>
2024-10-31 14:31:03 +0800

    fix bug

--- src/pages/right/indicator/SaveModal.tsx
+++ src/pages/right/indicator/SaveModal.tsx
@@ -73,7 +73,7 @@ export default function SaveModal(props: any) {
           <Form.Item label="指标描述" name={'desc'}>
             <Input.TextArea placeholder="请输入指标描述" rows={3} />
           </Form.Item>
-          <Form.Item label="所属业务过程" name={'buzProcess'}>
+          <Form.Item label="所属业务过程" name={'buzProcess'} rules={[{ required: true, message: '请选择所属业务过程' }]}>
             <Select placeholder="请选择所属业务过程" options={processOptions} disabled={!!editId}/>
           </Form.Item>
           <Form.Item label="相关业务过程">
